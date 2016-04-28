ddc382e4bbfccf3fef0b7fe8a755384ebdbeb700
wxs <wangxsh1984@163.com>
2016-04-28 17:28:53 +0800

    ajax

--- project/organization/WebContent/WEB-INF/jsp/demo/demo.jsp
+++ project/organization/WebContent/WEB-INF/jsp/demo/demo.jsp
@@ -2,8 +2,36 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
-<link type="text/css" rel="stylesheet" href="<%=request.getContextPath()%>/css/default/default.css">
 <title>demo</title>
+<link type="text/css" rel="stylesheet"
+	href="<%=request.getContextPath()%>/css/default/default.css">
+<script type="text/javascript"
+	src="<%=request.getContextPath()%>/js/jquery-1.7.js"></script>
+<script type="text/javascript">
+	function sendAjaxPost() {
+		$.post("<%=request.getContextPath()%>/demo/goAjax.do", {
+			Action : "post",
+			Name : "aaa"
+		}, function(data, textStatus) {
+			alert(data);
+		}, "json");
+	}
+	function ajaxTest() {
+		$.ajax({
+			data : "{Name:m模型}",
+			type : "POST",
+			dataType : 'json',
+			contentType: "application/json; charset=utf-8",
+			url : "<%=request.getContextPath()%>/demo/goAjax.do",
+			error : function(data) {
+				alert("出错了！！:" + data.msg);
+			},
+			success : function(data) {
+				alert(data.success);
+			}
+		});
+	}
+</script>
 </head>
 <body>
 	<h1>demo</h1>
@@ -12,4 +40,5 @@
 		<br />
 	</c:forEach>
 	<input type="text">
+	<button onclick="ajaxTest()">AJAX</button>
 </body>
--- project/organization/src/org/controller/demo/DemoController.java
+++ project/organization/src/org/controller/demo/DemoController.java
@@ -1,14 +1,23 @@
 package org.controller.demo;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.model.demo.DemoModel;
 import org.service.demo.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/demo")
@@ -18,10 +27,19 @@ public class DemoController {
 	private DemoService demoService;
 
 	@RequestMapping("/goDemo")
-	public String goDemo(Map map) {
+	public String goDemo(Map<String, List<DemoModel>> map) {
 		log.info("--------------------goDemo----------------------");
-		List<DemoModel> demoList = demoService.searchByQBC();//.searchDemo();
+		List<DemoModel> demoList = demoService.searchByQBC();// .searchDemo();
 		map.put("demoList", demoList);
 		return "demo/demo";
 	}
+
+	@ResponseBody
+	@RequestMapping(value = "/goAjax", method = RequestMethod.POST)
+	public String goAjax(@RequestBody String map) {
+		log.info("--------------------goAjax----------------------");
+		log.info((String) map);
+		return "{\"success\":\"aaa成功\"}";
+	}
+
 }
